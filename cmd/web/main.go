aa8482286444e8b973dd2692c5a531bce048a68b
apostorial <t.elouadrassi@gmail.com>
2025-12-04 22:30:17 +0100

    feat(server): add router with server setup

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/themoroccandemonlist/tmdl-server/internal/router"
+)
+
+func main() {
+	r := router.New()
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	go func() {
+		log.Printf("Starting server on :8080\n")
+		err := srv.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server failed: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err := srv.Shutdown(ctx)
+	if err != nil {
+		log.Printf("Server forced to shutdown: %v", err)
+	}
+
+	log.Printf("Server stopped.\n")
 }
---
+++ internal/router/router.go
@@ -0,0 +1,12 @@
+package router
+
+import (
+	"github.com/gorilla/mux"
+)
+
+func New() *mux.Router {
+
+	r := mux.NewRouter()
+
+	return r
+}
