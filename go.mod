244b062b4b50bfab021260665f3fda70163c34d4
apostorial <t.elouadrassi@gmail.com>
2025-12-04 22:13:57 +0100

    feat(config): add redis configuration

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/themoroccandemonlist/tmdl-server
 
 go 1.25.4
+
+require (
+	github.com/boj/redistore v1.4.1
+	github.com/gorilla/sessions v1.4.0
+	github.com/joho/godotenv v1.5.1
+)
+
+require (
+	github.com/gomodule/redigo v1.9.2 // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
+)
--- internal/config/config.go
+++ internal/config/config.go
@@ -2,9 +2,24 @@ package config
 
 import (
 	"log"
+	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
+
+	"github.com/boj/redistore"
+	"github.com/gorilla/sessions"
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Store *redistore.RediStore
+}
+
+const (
+	Production  = "PRODUCTION"
+	Development = "DEVELOPMENT"
 )
 
 func GetConfig(key string) string {
@@ -30,4 +45,51 @@ func ReadSecret(secret string) (string, error) {
 		return "", err
 	}
 	return strings.TrimSpace(string(data)), nil
+}
+
+func LoadRedis(sessionKey []byte, env string) *redistore.RediStore {
+	addr := GetConfig("REDIS_ADDRESS")
+	pass := GetConfig("REDIS_PASSWORD")
+	poolSize, _ := strconv.Atoi(GetConfig("REDIS_POOL_SIZE"))
+
+	store, err := redistore.NewRediStore(poolSize, "tcp", addr, "", pass, sessionKey)
+	if err != nil {
+		log.Fatal("Unable to create a RediStore: ", err)
+	}
+
+	secureCookies := env == Production
+
+	store.Options = &sessions.Options{
+		Path:     "/",
+		MaxAge:   86400 * 30,
+		HttpOnly: true,
+		Secure:   secureCookies,
+		SameSite: http.SameSiteLaxMode,
+	}
+
+	return store
+}
+
+func New() *Config {
+
+	var env string
+	err := godotenv.Load()
+	if err != nil {
+		log.Printf("Couldn't load .env, we must be in production.\n")
+		env = Production
+	} else {
+		env = Development
+	}
+
+	sessionKeyStr := GetConfig("SESSION_KEY")
+	var sessionKey []byte
+	if sessionKeyStr != "" {
+		sessionKey = []byte(sessionKeyStr)
+	}
+
+	redis := LoadRedis(sessionKey, env)
+
+	return &Config{
+		Store: redis,
+	}
 }
