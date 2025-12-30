863f708479d250d09e63b8dae9b7cb1ce531118d
apostorial <t.elouadrassi@gmail.com>
2025-12-31 00:33:46 +0100

    feat(html): added a temporary navbar design with daisyui

--- internal/middleware/content_security_policy.go
+++ internal/middleware/content_security_policy.go
@@ -5,7 +5,7 @@ import "net/http"
 func ContentSecurityPolicy(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Security-Policy",
-			"default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self'; font-src 'self'; connect-src 'self'; media-src 'self'; frame-src 'none'; frame-ancestors 'none'; object-src 'none'; base-uri 'self'; form-action 'self'; upgrade-insecure-requests; ")
+			"default-src 'self'; script-src 'self'; style-src 'self' https://cdn.jsdelivr.net/npm/daisyui@5.5.14 https://cdn.jsdelivr.net/npm/daisyui@5/themes.css; img-src 'self' data:; font-src 'self'; connect-src 'self'; media-src 'self'; frame-src 'none'; frame-ancestors 'none'; object-src 'none'; base-uri 'self'; form-action 'self'; upgrade-insecure-requests; ")
 		next.ServeHTTP(w, r)
 	})
 }
