f5751b8b27b7e23978f5179eaf2dd0ce650c0837
apostorial <t.elouadrassi@gmail.com>
2026-03-18 00:24:03 +0000

    chore(scripts): add alpinejs and rename htmx

--- internal/middleware/content_security_policy.go
+++ internal/middleware/content_security_policy.go
@@ -5,7 +5,7 @@ import "net/http"
 func ContentSecurityPolicy(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Security-Policy",
-			"default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self' data:; font-src 'self'; connect-src 'self'; media-src 'self'; frame-src 'none'; frame-ancestors 'none'; object-src 'none'; base-uri 'self'; form-action 'self'; upgrade-insecure-requests; ")
+			"default-src 'self'; script-src 'self' 'unsafe-eval'; style-src 'self' 'sha256-faU7yAF8NxuMTNEwVmBz+VcYeIoBQ2EMHW3WaVxCvnk=' https://fonts.googleapis.com; img-src 'self' data:; font-src 'self' https://fonts.gstatic.com; connect-src 'self'; media-src 'self'; frame-src 'none'; frame-ancestors 'none'; object-src 'none'; base-uri 'self'; form-action 'self'; upgrade-insecure-requests; ")
 		next.ServeHTTP(w, r)
 	})
 }
